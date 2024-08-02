d89eb67d6916ad3c73626fab07ae5fcee4804010
taewoo-dev <twin9774@gmail.com>
2024-08-02 13:42:44 +0900

    feat: lotto 번호 비교로직 추가

--- main.py
+++ main.py
@@ -3,41 +3,63 @@ from models.lotto import SelectLottoDto
 from database.db import DBManger
 
 
-def get_lotto_round(lotto_id: int):
-    lotto_id = int(input("회차를 입력").strip())
+def get_lotto_round(lotto_id: int) -> SelectLottoDto:
+    """
+    특정 회차의 로또 정보를 가져오는 함수
+    :return: SelectLottoDto
+    """
+    db = DBManger()
+
     sql = """
     select lotto_numbers, lotto_bonus, lotto_first, lotto_second, lotto_third, lotto_4th, lotto_5th 
     from lottos where lotto_id = %s;
     """
-    result = DBManger.excute_query(sql, 1129)
+    result = db.excute_query(sql, lotto_id)[0]
+
+    return SelectLottoDto(lotto_numbers=result["lotto_numbers"], lotto_bonus=result["lotto_bonus"],
+                          lotto_first=result["lotto_first"], lotto_second=result["lotto_second"], lotto_third=result["lotto_third"],
+                          lotto_4th=result["lotto_4th"], lotto_5th=result["lotto_5th"])
 
 
+def compare_lotto_numbers():
 
-def compare_lotto_numbers(lotto_number: list[str], lotto_amounts: list[str]):
-    count = 0
-    user_lotto_number = input("로또 번호를 6개 입력: ,로 구분").strip().split(",")
-    user_lotto_number.sort()
-    for i in user_lotto_number:
-        if i in lotto_number:
-            count = count + 1
+    lotto_id = int(input("회차를 입력").strip())
 
-    if count == 6:
-        print(f"1등 상금:{lotto_amounts[1]}")
-    elif count == 5:
-        print(f"2등 상금:{lotto_amounts[2]}")
-    elif count == 4:
-        print(f"3등 상금:{lotto_amounts[3]}")
-    elif count == 3:
-        print(f"4등 상금:{lotto_amounts[4]}")
+    select_lotto_dto = get_lotto_round(lotto_id=lotto_id)
+
+    lotto_numbers = select_lotto_dto.lotto_numbers.split(",")
+    lotto_bonus = select_lotto_dto.lotto_bonus
+
+    user_lotto_numbers = input("로또 번호를 6개 입력: ,로 구분").strip().split(",")
+    user_lotto_numbers.sort()
+
+    correct_count = 0
+    bonus = False
+
+    for user_lotto_number in user_lotto_numbers:
+        if user_lotto_number in lotto_numbers:
+            correct_count = correct_count + 1
+        if user_lotto_number == lotto_bonus:
+            bonus = True
+
+    if correct_count == 6:
+        print(f"축하합니다! 1등 당첨되었습니다 당첨금액은 {select_lotto_dto.lotto_first}입니다")
+    elif correct_count == 5 and bonus:
+        print(f"축하합니다! 2등 당첨되었습니다 당첨금액은 {select_lotto_dto.lotto_second}입니다")
+    elif correct_count == 5:
+        print(f"축하합니다! 3등 당첨되었습니다 당첨금액은 {select_lotto_dto.lotto_second}입니다")
+    elif correct_count == 4:
+        print(f"축하합니다! 4등 당첨되었습니다 당첨금액은 {select_lotto_dto.lotto_second}입니다")
+    elif correct_count == 3:
+        print(f"축하합니다! 5등 당첨되었습니다 당첨금액은 {select_lotto_dto.lotto_second}입니다")
     else:
-        print("0원")
+        print("아쉽지만 다음기회에")
 
-# (4). 로또 규칙 구체화
-# (5). 보너스 점수
+
 
 
 if __name__ == "__main__":
     # app = Crawler()
     # app.crawling_lottos()
-
+    compare_lotto_numbers()
 
--- models/lotto.py
+++ models/lotto.py
@@ -65,3 +65,33 @@ class SelectLottoDto:
         self._lotto_4th: str = lotto_4th
         self._lotto_5th: str = lotto_5th
 
+    @property
+    def lotto_numbers(self):
+        return self._lotto_numbers
+
+    @property
+    def lotto_bonus(self):
+        return self._lotto_bonus
+
+    @property
+    def lotto_first(self):
+        return self._lotto_first
+
+    @property
+    def lotto_second(self):
+        return self._lotto_second
+
+    @property
+    def lotto_third(self):
+        return self._lotto_third
+
+    @property
+    def lotto_4th(self):
+        return self._lotto_4th
+
+    @property
+    def lotto_5th(self):
+        return self._lotto_5th
+
+    def __str__(self):
+        return f"{self._lotto_numbers}, {self._lotto_bonus}, {self._lotto_first}"
